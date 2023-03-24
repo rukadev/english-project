098cd06ef58dcccebfbdd97299e1f69ef08b73a7
Jonathan Bogie <jmbogie@outlook.com>
2023-03-24 03:20:30 -0700

    updated map link

--- js/scripts/desc.js
+++ js/scripts/desc.js
@@ -3,6 +3,6 @@ var buttons = document.querySelectorAll('button')
 buttons.forEach(button => {
     button.onclick = function() {
         sessionStorage.setItem('gameName', button.innerHTML)
-        window.location.href = "../../html/map.html"
+        window.location.href = "map.html"
     }
 });
